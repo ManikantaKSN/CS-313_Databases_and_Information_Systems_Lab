3b254bd6ac63bd796adc4aa4428c70ae6dc838ee
ManikantaKSN <manikantaksn28@gmail.com>
2023-10-03 22:55:53 +0530

    Modified q3

--- Lab4/q3.py
+++ Lab4/q3.py
@@ -5,18 +5,22 @@ conn = psycopg2.connect(database='moviedb',
                         user="postgres",
                         password="pg23",
                         port=5432)
-actor_id = int(input("Enter actor id: "))
+
+mycursor = conn.cursor()
+# Assuming that actor ids are entered in serial order or increasing order, i.e
+# act_id of an actor inserted later is greater in value than that of an actor inserted earlier.
+mycursor.execute("SELECT MAX(act_id) FROM actor")
+actor_id = mycursor.fetchone()[0]
+print("For actor_id =",actor_id)
 no_of_movies = int(input("Enter the number of movies: "))
 movies_info = []
 success = True
 
-for i in range(1, no_of_movies + 1):
-    movie_id = int(input(f"Enter movie id of movie number {i}: "))
-    role = input(f"Enter role of the actor in movie number {i}: ")
+for i in range(no_of_movies):
+    movie_id = int(input(f"Enter movie id of movie number {i+1}: "))
+    role = input(f"Enter role of the actor in movie number {i+1}: ")
     movies_info.append((movie_id, role))
     
-mycursor = conn.cursor()
-
 for i, (movie_id, role) in enumerate(movies_info):
     mycursor.execute("SELECT * FROM movie WHERE mov_id = %s", (movie_id,))
     exists = mycursor.fetchone()
