c323f1b6e2fba93d78f36419bbbcecf0b78c668c
ManikantaKSN <manikantaksn28@gmail.com>
2023-10-04 18:02:05 +0530

    Small Changes done

--- Lab4/q3.py
+++ Lab4/q3.py
@@ -7,11 +7,16 @@ conn = psycopg2.connect(database='moviedb',
                         port=5432)
 
 mycursor = conn.cursor()
-# Assuming that actor ids are entered in serial order or increasing order, i.e
-# act_id of an actor inserted later is greater in value than that of an actor inserted earlier.
+print()
+print("""Assumption: actor ids are entered in serial order or increasing order, i.e
+act_id of an actor inserted later is greater in value than that of an actor inserted earlier.
+So, we expect the user to insert into 'movie_cast' relation, the roles played by the actor in the movies present
+in the database right after a new actor is inserted into the 'actor' relation(i.e right after running q1.py) =>""")
+print()
 mycursor.execute("SELECT MAX(act_id) FROM actor")
 actor_id = mycursor.fetchone()[0]
-print("For actor_id : ",actor_id)
+print("Latest(inserted) actor_id :",actor_id)
+print()
 no_of_movies = int(input("Enter the number of movies: "))
 movies_info = []
 success = True
